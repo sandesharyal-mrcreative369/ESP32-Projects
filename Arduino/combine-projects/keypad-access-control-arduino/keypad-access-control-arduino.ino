06d50f0f8f22ed21b7ce239fd88ebe6775f9cb01
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-04-22 22:26:36 +0530

    adding OLED Display Program File

--- Arduino/combine-projects/keypad-access-control-arduino/keypad-access-control-arduino.ino
+++ Arduino/combine-projects/keypad-access-control-arduino/keypad-access-control-arduino.ino
@@ -1,5 +1,15 @@
 #include<Keypad.h>
 
+#include<Wire.h>
+#include<Adafruit_SSD1306.h>
+#include<Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 oled(128,64,&Wire,-1);
+
+
 const byte ROWS = 4;
 const byte COLS = 4;
 
@@ -19,15 +29,34 @@ Keypad customKeyPad = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
 void setup(){
   Serial.begin(9600);
 
+  if(!oled.begin(SSD1306_SWITCHCAPVCC,0x3C)){
+    Serial.print("Failed to Connect...");
+    while(true);
+  }
+
+  oled.clearDisplay();
+
 
 }
 
 void loop(){
+
+    oled.clearDisplay();
+    oled.setTextSize(2);
+    oled.setTextColor(WHITE);
+
+    oled.setCursor(5,10);
+    oled.print("Password: ");
+
     char customKeys = customKeyPad.getKey();
 
     if(customKeys){
-        Serial.println(customKeys);
+        
 
     }
+
+    oled.display();
+
+    delay(400);
 
 }
